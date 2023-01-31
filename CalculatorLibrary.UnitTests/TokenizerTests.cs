194815aa0ab82357a50826bb50921128304cd9c0
Frognar <sebastian.przyszlak@gmail.com>
2023-01-31 22:09:19 +0100

    create tokenizer class

---
+++ CalculatorLibrary.UnitTests/TokenizerTests.cs
@@ -0,0 +1,10 @@
+namespace CalculatorLibrary.UnitTests;
+
+public class TokenizerTests
+{
+    [Fact]
+    public void Tokenizer_CanBeCreated()
+    {
+        Tokenizer tokenizer = new();
+    }
+}
