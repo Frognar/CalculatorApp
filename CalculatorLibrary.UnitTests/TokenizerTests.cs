772e04506818006280f9784f25fe8dd7a138af5e
Frognar <sebastian.przyszlak@gmail.com>
2023-01-31 22:22:04 +0100

    delete tests that are no longer needed

--- CalculatorLibrary.UnitTests/TokenizerTests.cs
+++ CalculatorLibrary.UnitTests/TokenizerTests.cs
@@ -3,29 +3,6 @@ namespace CalculatorLibrary.UnitTests;
 public class TokenizerTests
 {
     [Fact]
-    public void Tokenizer_CanBeCreated()
-    {
-        Tokenizer tokenizer = new();
-    }
-
-    [Fact]
-    public void Token_CanBeCreated()
-    {
-        Token token = new()
-        {
-            Type = TokenType.Stop,
-            Text = "",
-        };
-    }
-
-    [Fact]
-    public void Tokenize_ReturnListOfTokens()
-    {
-        Tokenizer tokenizer = new();
-        IEnumerable<Token> tokens = tokenizer.Tokenize("");
-    }
-
-    [Fact]
     public void Tokenize_Empty_ReturnsListContainingOnlyStopToken()
     {
         Tokenizer tokenizer = new();
