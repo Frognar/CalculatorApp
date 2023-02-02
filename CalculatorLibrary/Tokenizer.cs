250a4a14ddf013578de6956c70dd447d9fed370e
Frognar <sebastian.przyszlak@gmail.com>
2023-02-02 18:15:33 +0100

    refactor tokenize method

--- CalculatorLibrary/Tokenizer.cs
+++ CalculatorLibrary/Tokenizer.cs
@@ -1,78 +1,54 @@
+using System.Text.RegularExpressions;
+
 namespace CalculatorLibrary;
 
 public class Tokenizer
 {
-    enum State { None, Number, Operator }
-    readonly char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
     readonly List<Token> tokens = new();
     int position;
-    State state;
-    int tokenPosition;
     
     public IEnumerable<Token> Tokenize(string expression)
     {
         tokens.Clear();
-        state = State.None;
-        tokenPosition = 0;
-        for (position = 0; position < expression.Length; position++)
+        for (position = 0; position < expression.Length;)
             TokenizeExpression(expression);
 
         tokens.Add(Token.Stop);
         return tokens;
     }
 
+    static readonly Regex WhiteRegex = new("^\\s+");
+    static readonly Regex NumberRegex = new("^[0-9]+");
     void TokenizeExpression(string expression)
     {
-        switch (state)
+        Match matcher = WhiteRegex.Match(expression[position..]);
+        if (matcher.Success)
         {
-            case State.None:
-                if (expression[position] == ' ')
-                    break;
-
-                tokenPosition = position;
-                state = digits.Contains(expression[position]) ? State.Number : State.Operator;
-                break;
-
-            case State.Number:
-                if (digits.Contains(expression[position]))
-                    break;
-
-                tokens.Add(new Token
-                {
-                    Type = TokenType.Number,
-                    Text = expression[tokenPosition..position]
-                });
-
-                tokenPosition = position;
-                state = expression[position] == ' ' ? State.None : State.Operator;
-                break;
-
-            case State.Operator:
+            position += matcher.Length;
+            return;
+        }
+        
+        switch (expression[position])
+        {
+            case '+':
                 tokens.Add(new Token
                 {
                     Type = TokenType.Operator,
-                    Text = expression[tokenPosition..position]
+                    Text = expression.Substring(position, 1)
                 });
-
-                tokenPosition = position;
-                if (expression[position] == ' ')
-                    state = State.None;
-                else if (digits.Contains(expression[position]))
-                    state = State.Number;
-
-                break;
+                position++;
+                return;
         }
-
-        if (position == expression.Length - 1 && state != State.None)
+        
+        matcher = NumberRegex.Match(expression[position..]);
+        if (matcher.Success)
+        {
             tokens.Add(new Token
             {
-                Type = state switch
-                {
-                    State.Number => TokenType.Number,
-                    State.Operator => TokenType.Operator,
-                    _ => throw new ArgumentException()
-                },
-                Text = expression[tokenPosition..]
+                Type = TokenType.Number,
+                Text = expression.Substring(position, matcher.Length)
             });
+            position += matcher.Length;
+        }
     }
 }
