3f372cc5acaa425884fa785522d6401e40b907df
Frognar <sebastian.przyszlak@gmail.com>
2023-02-02 17:59:37 +0100

    extract loop body to method

--- CalculatorLibrary/Tokenizer.cs
+++ CalculatorLibrary/Tokenizer.cs
@@ -15,61 +15,64 @@ public class Tokenizer
         state = State.None;
         tokenPosition = 0;
         for (position = 0; position < expression.Length; position++)
-        {
-            switch (state)
-            {
-                case State.None:
-                    if (expression[position] == ' ')
-                        break;
-                    
-                    tokenPosition = position;
-                    state = digits.Contains(expression[position]) ? State.Number : State.Operator;
-                    break;
-                
-                case State.Number:
-                    if (digits.Contains(expression[position]))
-                        break;
-                    
-                    tokens.Add(new Token
-                    {
-                        Type = TokenType.Number,
-                        Text = expression[tokenPosition..position]
-                    });
-                    
-                    tokenPosition = position;
-                    state = expression[position] == ' ' ? State.None : State.Operator;
-                    break;
-                
-                case State.Operator:
-                    tokens.Add(new Token
-                    {
-                        Type = TokenType.Operator,
-                        Text = expression[tokenPosition..position]
-                    });
-                    
-                    tokenPosition = position;
-                    if (expression[position] == ' ')
-                        state = State.None;
-                    else if (digits.Contains(expression[position]))
-                        state = State.Number;
-                    
-                    break;
-            }
-            
-            if (position == expression.Length - 1 && state != State.None)
-                tokens.Add(new Token
-                {
-                    Type = state switch
-                    {
-                        State.Number => TokenType.Number,
-                        State.Operator => TokenType.Operator,
-                        _ => throw new ArgumentException()
-                    },
-                    Text = expression[tokenPosition..]
-                });
-        }
+            TokenizeExpression(expression);
 
         tokens.Add(Token.Stop);
         return tokens;
+    }
+
+    void TokenizeExpression(string expression)
+    {
+        switch (state)
+        {
+            case State.None:
+                if (expression[position] == ' ')
+                    break;
+
+                tokenPosition = position;
+                state = digits.Contains(expression[position]) ? State.Number : State.Operator;
+                break;
+
+            case State.Number:
+                if (digits.Contains(expression[position]))
+                    break;
+
+                tokens.Add(new Token
+                {
+                    Type = TokenType.Number,
+                    Text = expression[tokenPosition..position]
+                });
+
+                tokenPosition = position;
+                state = expression[position] == ' ' ? State.None : State.Operator;
+                break;
+
+            case State.Operator:
+                tokens.Add(new Token
+                {
+                    Type = TokenType.Operator,
+                    Text = expression[tokenPosition..position]
+                });
+
+                tokenPosition = position;
+                if (expression[position] == ' ')
+                    state = State.None;
+                else if (digits.Contains(expression[position]))
+                    state = State.Number;
+
+                break;
+        }
+
+        if (position == expression.Length - 1 && state != State.None)
+            tokens.Add(new Token
+            {
+                Type = state switch
+                {
+                    State.Number => TokenType.Number,
+                    State.Operator => TokenType.Operator,
+                    _ => throw new ArgumentException()
+                },
+                Text = expression[tokenPosition..]
+            });
     }
 }
