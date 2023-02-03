2bd740be5a17d3f2e494e13efb560e03103c9761
Frognar <sebastian.przyszlak@gmail.com>
2023-02-03 21:47:44 +0100

    delete tests that are no longer needed

--- CalculatorLibrary.UnitTests/ParserTests.cs
+++ CalculatorLibrary.UnitTests/ParserTests.cs
@@ -3,20 +3,6 @@ namespace CalculatorLibrary.UnitTests;
 public class ParserTests
 {
     [Fact]
-    public void CanCreateParser()
-    {
-        Parser parser = new();
-    }
-
-    [Fact]
-    public void ParserCanParseTokens()
-    {
-        Parser parser = new();
-
-        decimal _ = parser.Parse(Enumerable.Empty<Token>());
-    }
-
-    [Fact]
     public void Parse_EmptyCollection_ReturnZero()
     {
         Parser parser = new();
