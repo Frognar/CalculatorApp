a81475af0da08193b2b2724796acca9fa5e8f55a
Frognar <sebastian.przyszlak@gmail.com>
2023-02-12 09:18:37 +0100

    extract finding single character tokens

--- MathCalc/Lexer.cs
+++ MathCalc/Lexer.cs
@@ -18,63 +18,7 @@ public class Lexer
         FindWhiteSpaces(expression);
 
         if (position < expression.Length)
-        {
-            switch (expression[position])
-            {
-                case '{':
-                    tokenCollector.OpenBrace(0, position);
-                    position++;
-                    break;
-                case '}':
-                    tokenCollector.ClosedBrace(0, position);
-                    position++;
-                    break;
-                case '(':
-                    tokenCollector.OpenParen(0, position);
-                    position++;
-                    break;
-                case ')':
-                    tokenCollector.ClosedParen(0, position);
-                    position++;
-                    break;
-                case '<':
-                    tokenCollector.OpenAngle(0, position);
-                    position++;
-                    break;
-                case '>':
-                    tokenCollector.ClosedAngle(0, position);
-                    position++;
-                    break;
-                case '-':
-                    tokenCollector.MinusSign(0, position);
-                    position++;
-                    break;
-                case '+':
-                    tokenCollector.PlusSign(0, position);
-                    position++;
-                    break;
-                case '^':
-                    tokenCollector.ExponentSymbol(0, position);
-                    position++;
-                    break;
-                case '*':
-                    tokenCollector.Asterisk(0, position);
-                    position++;
-                    break;
-                case '/':
-                    tokenCollector.Slash(0, position);
-                    position++;
-                    break;
-                case ',':
-                    tokenCollector.Comma(0, position);
-                    position++;
-                    break;
-                case '%':
-                    tokenCollector.PercentSing(0, position);
-                    position++;
-                    break;
-            }
-        }
+            FindSingleCharacterToken(expression);
 
         Match match = Regex.Match(expression, "^\\w+");
         if (match.Success)
@@ -89,5 +33,64 @@ public class Lexer
         Match wsMatch = Regex.Match(line, "^\\s+");
         if (wsMatch.Success)
             position += wsMatch.Length;
+    }
+
+    void FindSingleCharacterToken(string line)
+    {
+        switch (line[position])
+        {
+            case '{':
+                tokenCollector.OpenBrace(0, position);
+                position++;
+                break;
+            case '}':
+                tokenCollector.ClosedBrace(0, position);
+                position++;
+                break;
+            case '(':
+                tokenCollector.OpenParen(0, position);
+                position++;
+                break;
+            case ')':
+                tokenCollector.ClosedParen(0, position);
+                position++;
+                break;
+            case '<':
+                tokenCollector.OpenAngle(0, position);
+                position++;
+                break;
+            case '>':
+                tokenCollector.ClosedAngle(0, position);
+                position++;
+                break;
+            case '-':
+                tokenCollector.MinusSign(0, position);
+                position++;
+                break;
+            case '+':
+                tokenCollector.PlusSign(0, position);
+                position++;
+                break;
+            case '^':
+                tokenCollector.ExponentSymbol(0, position);
+                position++;
+                break;
+            case '*':
+                tokenCollector.Asterisk(0, position);
+                position++;
+                break;
+            case '/':
+                tokenCollector.Slash(0, position);
+                position++;
+                break;
+            case ',':
+                tokenCollector.Comma(0, position);
+                position++;
+                break;
+            case '%':
+                tokenCollector.PercentSing(0, position);
+                position++;
+                break;
+        }
     }
 }
