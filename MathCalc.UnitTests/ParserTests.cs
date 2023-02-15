94075ed14bd4a26f33e6f020fe0beeec39b81daf
Frognar <sebastian.przyszlak@gmail.com>
2023-02-15 13:10:04 +0100

    refactor tests

--- MathCalc.UnitTests/ParserTests.cs
+++ MathCalc.UnitTests/ParserTests.cs
@@ -1,4 +1,5 @@
 using Frognar.MathCalc;
+using Frognar.MathCalc.Enums;
 
 namespace MathCalc.UnitTests;
 
@@ -15,123 +16,100 @@ public class ParserTests
         lexer = new Lexer(parser);
     }
 
+    void AssertParseResult(string expression, string expected)
+    {
+        lexer.Lex(expression);
+
+        Assert.Equal(expected, builder.GetExpression());
+    }
+    
     [Fact]
     public void Parse_X()
     {
-        lexer.Lex("123");
-        
-        Assert.Equal("123", builder.GetExpression());
+        AssertParseResult("123", "123");
     }
 
     [Fact]
     public void Parse_MinusX()
     {
-        lexer.Lex("-123");
-        
-        Assert.Equal("123 ~", builder.GetExpression());
+        AssertParseResult("-123", "123 ~");
     }
 
     [Fact]
     public void Parse_XMinusY()
     {
-        lexer.Lex("123 - 23");
-        
-        Assert.Equal("123 23 -", builder.GetExpression());
+        AssertParseResult("123 - 23", "123 23 -");
     }
 
     [Fact]
     public void Parse_XPlusY()
     {
-        lexer.Lex("100 + 23");
-        
-        Assert.Equal("100 23 +", builder.GetExpression());
+        AssertParseResult("100 + 23", "100 23 +");
     }
 
     [Fact]
     public void Parse_XPlusMuinusY()
     {
-        lexer.Lex("100 + -23");
-        
-        Assert.Equal("100 23 ~ +", builder.GetExpression());
+        AssertParseResult("100 + -23", "100 23 ~ +");
     }
 
     [Fact]
     public void Parse_XTimesY()
     {
-        lexer.Lex("10 * 5");
-        
-        Assert.Equal("10 5 *", builder.GetExpression());
+        AssertParseResult("10 * 5", "10 5 *");
     }
 
     [Fact]
     public void Parse_XTimesYPlusZ()
     {
-        lexer.Lex("10 * 2 + 5");
-        
-        Assert.Equal("10 2 * 5 +", builder.GetExpression());
+        AssertParseResult("10 * 2 + 5", "10 2 * 5 +");
     }
 
     [Fact]
     public void Parse_XDevidedByY()
     {
-        lexer.Lex("10 / 2");
-        
-        Assert.Equal("10 2 /", builder.GetExpression());
+        AssertParseResult("10 / 2", "10 2 /");
     }
 
     [Fact]
     public void Parse_XDevidedByMinusY()
     {
-        lexer.Lex("10 / -2");
-        
-        Assert.Equal("10 2 ~ /", builder.GetExpression());
+        AssertParseResult("10 / -2", "10 2 ~ /");
     }
 
     [Fact]
     public void Parse_XTimesMinusY()
     {
-        lexer.Lex("10 * -2");
-        
-        Assert.Equal("10 2 ~ *", builder.GetExpression());
+        AssertParseResult("10 * -2", "10 2 ~ *");
     }
 
     [Fact]
     public void Parse_XTimexYPlusMinusZ()
     {
-        lexer.Lex("10 * 2 + -5");
-        
-        Assert.Equal("10 2 * 5 ~ +", builder.GetExpression());
+        AssertParseResult("10 * 2 + -5", "10 2 * 5 ~ +");
     }
 
     [Fact]
     public void Parse_XToPowerOfY()
     {
-        lexer.Lex("10 ^ 2");
-        
-        Assert.Equal("10 2 ^", builder.GetExpression());
+        AssertParseResult("10 ^ 2", "10 2 ^");
     }
 
     [Fact]
     public void Parse_XToPowerOfMinusY()
     {
-        lexer.Lex("10 ^ -2");
-        
-        Assert.Equal("10 2 ~ ^", builder.GetExpression());
+        AssertParseResult("10 ^ -2", "10 2 ~ ^");
     }
 
     [Fact]
     public void Parse_XTimeYPlusZInParen()
     {
-        lexer.Lex("10 * (1 + 1)");
-        
-        Assert.Equal("10 1 1 + *", builder.GetExpression());
+        AssertParseResult("10 * (1 + 1)", "10 1 1 + *");
     }
 
     [Fact]
     public void Parse_ComplexExpression()
     {
-        lexer.Lex("10 + ( 3 * 2 ) ^ 2 ^ -3 - 25 / 5");
-        
-        Assert.Equal("10 3 2 * 2 3 ~ ^ ^ + 25 5 / -", builder.GetExpression());
+        AssertParseResult("10 + ( 3 * 2 ) ^ 2 ^ -3 - 25 / 5", "10 3 2 * 2 3 ~ ^ ^ + 25 5 / -");
     }
 }
