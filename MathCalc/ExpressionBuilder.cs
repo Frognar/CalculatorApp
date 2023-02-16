0bacdcabac142ff42f56fa45990131dad7d7417d
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2023-02-16 20:21:40 +0100

    error for missing ')'

--- MathCalc/ExpressionBuilder.cs
+++ MathCalc/ExpressionBuilder.cs
@@ -5,6 +5,7 @@ namespace Frognar.MathCalc;
 
 public class ExpressionBuilder : Builder
 {
+    int parens;
     readonly Expression expression = new();
     public Expression GetExpression() => expression;
     public void SetNumber(string number) => expression.AddNumber(number);
@@ -14,9 +15,26 @@ public class ExpressionBuilder : Builder
     public void SetAsterisk() => expression.AddOperator("*");
     public void SetSlash() => expression.AddOperator("/");
     public void SetExponentSymbol() => expression.AddOperator("^");
-    public void SetOpenParen() => expression.AddOperator("(");
-    public void SetClosedParen() => expression.AddOperator(")");
-    public void CompleteExpression() => expression.Complete();
+    public void SetOpenParen()
+    {
+        parens++;
+        expression.AddOperator("(");
+    }
+
+    public void SetClosedParen()
+    {
+        parens--;
+        expression.AddOperator(")");
+    }
+
+    public void CompleteExpression()
+    {
+        if (parens > 0)
+            expression.AddError($"Syntax error: Expr. Missing {parens} ')'");
+        
+        expression.Complete();
+    }
+
     public void SetExprError(ParserState state, ParserEvent parserEvent, int line, int position)
         => expression.AddError($"Syntax error: Expr. {state}|{parserEvent}. line {line}, position {position}.");
 }
