32c730b4c5d9fa359c2fffbf9d88ab827cb389d8
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2023-02-16 20:09:09 +0100

    more error tests + start expr with paren

--- MathCalc.UnitTests/ParserTests.cs
+++ MathCalc.UnitTests/ParserTests.cs
@@ -121,6 +121,12 @@ public class ParserTests
         {
             AssertParseResult("10 + ( 3 * 2 ) ^ 2 ^ -3 - 25 / 5", "10 3 2 * 2 3 ~ ^ ^ + 25 5 / -");
         }
+
+        [Fact]
+        public void Parse_StartWithParen()
+        {
+            AssertParseResult("(1 + 1) * 2", "1 1 + 2 *");
+        }
     }
 
     public class ErrorTests : ParserTests
@@ -129,6 +135,24 @@ public class ParserTests
         public void Parse_Nothing()
         {
             AssertParseError("", "Syntax error: Expr. Expr|EOF. line -1, position -1.");
+        }
+
+        [Fact]
+        public void Parse_TwoNumbersInRow()
+        {
+            AssertParseError("12 12", "Syntax error: Expr. Number|Number. line 1, position 3.");
+        }
+
+        [Fact]
+        public void Parse_OperatorOnStart()
+        {
+            AssertParseError("^ 12", "Syntax error: Expr. Expr|ExponentSymbol. line 1, position 0.");
+        }
+
+        [Fact]
+        public void Parse_ClosedParamAfterOpenParam()
+        {
+            AssertParseError("()", "Syntax error: Expr. Operator|ClosedParen. line 1, position 1.");
         }
     }
 }
