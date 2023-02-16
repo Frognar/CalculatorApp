4ac3ab4384ccafb40f5630662c061d903aed602a
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2023-02-16 19:20:11 +0100

    move parser tests to nested class

--- MathCalc.UnitTests/ParserTests.cs
+++ MathCalc.UnitTests/ParserTests.cs
@@ -1,5 +1,4 @@
 using Frognar.MathCalc;
-using Frognar.MathCalc.Enums;
 
 namespace MathCalc.UnitTests;
 
@@ -19,97 +18,100 @@ public class ParserTests
     void AssertParseResult(string expression, string expected)
     {
         lexer.Lex(expression);
-
+        
         Assert.Equal(expected, builder.GetExpression());
     }
-    
-    [Fact]
-    public void Parse_X()
-    {
-        AssertParseResult("123", "123");
-    }
 
-    [Fact]
-    public void Parse_MinusX()
+    public class IncrementalTests : ParserTests
     {
-        AssertParseResult("-123", "123 ~");
-    }
+        [Fact]
+        public void Parse_X()
+        {
+            AssertParseResult("123", "123");
+        }
 
-    [Fact]
-    public void Parse_XMinusY()
-    {
-        AssertParseResult("123 - 23", "123 23 -");
-    }
+        [Fact]
+        public void Parse_MinusX()
+        {
+            AssertParseResult("-123", "123 ~");
+        }
 
-    [Fact]
-    public void Parse_XPlusY()
-    {
-        AssertParseResult("100 + 23", "100 23 +");
-    }
+        [Fact]
+        public void Parse_XMinusY()
+        {
+            AssertParseResult("123 - 23", "123 23 -");
+        }
 
-    [Fact]
-    public void Parse_XPlusMuinusY()
-    {
-        AssertParseResult("100 + -23", "100 23 ~ +");
-    }
+        [Fact]
+        public void Parse_XPlusY()
+        {
+            AssertParseResult("100 + 23", "100 23 +");
+        }
 
-    [Fact]
-    public void Parse_XTimesY()
-    {
-        AssertParseResult("10 * 5", "10 5 *");
-    }
+        [Fact]
+        public void Parse_XPlusMuinusY()
+        {
+            AssertParseResult("100 + -23", "100 23 ~ +");
+        }
 
-    [Fact]
-    public void Parse_XTimesYPlusZ()
-    {
-        AssertParseResult("10 * 2 + 5", "10 2 * 5 +");
-    }
+        [Fact]
+        public void Parse_XTimesY()
+        {
+            AssertParseResult("10 * 5", "10 5 *");
+        }
 
-    [Fact]
-    public void Parse_XDevidedByY()
-    {
-        AssertParseResult("10 / 2", "10 2 /");
-    }
+        [Fact]
+        public void Parse_XTimesYPlusZ()
+        {
+            AssertParseResult("10 * 2 + 5", "10 2 * 5 +");
+        }
 
-    [Fact]
-    public void Parse_XDevidedByMinusY()
-    {
-        AssertParseResult("10 / -2", "10 2 ~ /");
-    }
+        [Fact]
+        public void Parse_XDevidedByY()
+        {
+            AssertParseResult("10 / 2", "10 2 /");
+        }
 
-    [Fact]
-    public void Parse_XTimesMinusY()
-    {
-        AssertParseResult("10 * -2", "10 2 ~ *");
-    }
+        [Fact]
+        public void Parse_XDevidedByMinusY()
+        {
+            AssertParseResult("10 / -2", "10 2 ~ /");
+        }
 
-    [Fact]
-    public void Parse_XTimexYPlusMinusZ()
-    {
-        AssertParseResult("10 * 2 + -5", "10 2 * 5 ~ +");
-    }
+        [Fact]
+        public void Parse_XTimesMinusY()
+        {
+            AssertParseResult("10 * -2", "10 2 ~ *");
+        }
 
-    [Fact]
-    public void Parse_XToPowerOfY()
-    {
-        AssertParseResult("10 ^ 2", "10 2 ^");
-    }
+        [Fact]
+        public void Parse_XTimexYPlusMinusZ()
+        {
+            AssertParseResult("10 * 2 + -5", "10 2 * 5 ~ +");
+        }
 
-    [Fact]
-    public void Parse_XToPowerOfMinusY()
-    {
-        AssertParseResult("10 ^ -2", "10 2 ~ ^");
-    }
+        [Fact]
+        public void Parse_XToPowerOfY()
+        {
+            AssertParseResult("10 ^ 2", "10 2 ^");
+        }
 
-    [Fact]
-    public void Parse_XTimeYPlusZInParen()
-    {
-        AssertParseResult("10 * (1 + 1)", "10 1 1 + *");
-    }
+        [Fact]
+        public void Parse_XToPowerOfMinusY()
+        {
+            AssertParseResult("10 ^ -2", "10 2 ~ ^");
+        }
 
-    [Fact]
-    public void Parse_ComplexExpression()
-    {
-        AssertParseResult("10 + ( 3 * 2 ) ^ 2 ^ -3 - 25 / 5", "10 3 2 * 2 3 ~ ^ ^ + 25 5 / -");
+        [Fact]
+        public void Parse_XTimeYPlusZInParen()
+        {
+            AssertParseResult("10 * (1 + 1)", "10 1 1 + *");
+        }
+
+        [Fact]
+        public void Parse_ComplexExpression()
+        {
+            AssertParseResult("10 + ( 3 * 2 ) ^ 2 ^ -3 - 25 / 5", "10 3 2 * 2 3 ~ ^ ^ + 25 5 / -");
+        }
     }
 }
