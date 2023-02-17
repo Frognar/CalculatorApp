ef45a72ecaeb2ff60f87ca98efe837f912bed7a9
Frognar <sebastian.przyszlak@gmail.com>
2023-02-17 14:48:02 +0100

    create evaluator

---
+++ MathCalc.UnitTests/EvaluatorTests.cs
@@ -0,0 +1,13 @@
+﻿using Frognar.MathCalc;
+using Frognar.MathCalc.Expressions;
+
+namespace MathCalc.UnitTests;
+
+public class EvaluatorTests
+{
+    [Fact]
+    public void CanCreateEvaluator()
+    {
+        Evaluator _ = new(new Expression());
+    }
+}
---
+++ MathCalc/Evaluator.cs
@@ -0,0 +1,10 @@
+﻿using Frognar.MathCalc.Expressions;
+
+namespace Frognar.MathCalc;
+
+public class Evaluator
+{
+    public Evaluator(Expression expression)
+    {
+    }
+}
