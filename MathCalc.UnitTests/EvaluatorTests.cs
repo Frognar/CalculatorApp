6708b123da92d0b98748590a40da4f902466159b
Frognar <sebastian.przyszlak@gmail.com>
2023-02-17 15:12:17 +0100

    use lexer, parser and builder in evaluator tests

--- MathCalc.UnitTests/EvaluatorTests.cs
+++ MathCalc.UnitTests/EvaluatorTests.cs
@@ -1,16 +1,29 @@
 ﻿using System.Data;
 using Frognar.MathCalc;
+using Frognar.MathCalc.Enums;
 using Frognar.MathCalc.Expressions;
 
 namespace MathCalc.UnitTests;
 
 public class EvaluatorTests
 {
+    Lexer lexer;
+    Parser parser;
+    Builder builder;
+
+    public EvaluatorTests()
+    {
+        builder = new ExpressionBuilder();
+        parser = new Parser(builder);
+        lexer = new Lexer(parser);
+    }
+    
     [Fact]
     public void Evaluate_ExpressionWithErrors_ThrowInvalidExpressionException()
     {
-        Expression expression = new();
-        expression.AddError("ERROR");
+        lexer.Lex("");
+        parser.HandleEvent(ParserEvent.EOF, -1, -1);
+        Expression expression = builder.GetExpression();
         Evaluator evaluator = new(expression);
 
         Assert.Throws<InvalidExpressionException>(() => evaluator.Evaluate());
@@ -19,8 +32,9 @@ public class EvaluatorTests
     [Fact]
     public void Evaluate_ExpressionWithSingleNumber()
     {
-        Expression expression = new();
-        expression.AddNumber("123");
+        lexer.Lex("123");
+        parser.HandleEvent(ParserEvent.EOF, -1, -1);
+        Expression expression = builder.GetExpression();
         Evaluator evaluator = new(expression);
         
         Assert.Equal(123d, evaluator.Evaluate(), 0.0001);
