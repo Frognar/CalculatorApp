c5f6400592c7d5ec457779a8f62bf9a17a4eb0d6
Frognar <sebastian.przyszlak@gmail.com>
2023-02-18 09:21:56 +0100

    evaluate single expression

--- MathCalc.UnitTests/CalculatorTests.cs
+++ MathCalc.UnitTests/CalculatorTests.cs
@@ -9,4 +9,12 @@ public class CalculatorTests
     {
         Calculator c = new();
     }
+
+    [Fact]
+    public void Evaluate_EvaluatesExpression()
+    {
+        Calculator calculator = new();
+        
+        Assert.Equal(1679601d, calculator.Evaluate("-10 + ( 3 * 2 ) ^ 2 ^ 3 - 25 / 5"), 0.001);
+    }
 }
--- MathCalc/Calculator.cs
+++ MathCalc/Calculator.cs
@@ -1,6 +1,20 @@
-﻿namespace Frognar.MathCalc;
+﻿using Frognar.MathCalc.Enums;
+using Frognar.MathCalc.Expressions;
+
+namespace Frognar.MathCalc;
 
 public class Calculator
 {
-    
+    public double Evaluate(string expression)
+    {
+        Builder builder = new ExpressionBuilder();
+        Parser parser = new(builder);
+        Lexer lexer = new(parser);
+        
+        lexer.Lex(expression);
+        parser.HandleEvent(ParserEvent.EOF, -1, -1);
+
+        Evaluator evaluator = new(builder.GetExpression());
+        return evaluator.Evaluate();
+    }
 }
